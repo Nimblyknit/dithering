0d1a64cb3c092ba98cfb4a7c13e043a5ab635f0c
NimblyKnit <nimblyas@sent.com>
2026-09-22 11:20:34 +1000

    Update ImageSelectionScreen.svelte
    Corrected missing’

--- src/routes/ImageSelectionScreen.svelte
+++ src/routes/ImageSelectionScreen.svelte
@@ -3,7 +3,7 @@
 	import cat_img_src from './images/cat.jpg';
 	import david_img_src from './images/david.png';
 	import radial_gradient_img_src from './images/radial.png';
-    import knitted_img_src from './images/knitted-dithering.png;
+    import knitted_img_src from './images/knitted-dithering.png';
 	import { loadImageFile } from './utils';
 	import { Icon, Plus } from 'svelte-hero-icons';
 	import logo_src from '$lib/assets/dither-studio-logo.png';
